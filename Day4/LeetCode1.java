38e2ba3b926bb34659b707aeb47f07e13b266a20
Ackqueen <ackqueensj@gmail.com>
2026-03-24 18:28:20 +0530

    Day$

---
+++ Day4/LeetCode1.java
@@ -0,0 +1,22 @@
+class LeetCode1 {
+    public int[] twoSum(int[] nums, int target) {
+        int []result = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        LeetCode1 solution = new LeetCode1();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
---
+++ Day4Task/LeetCode217.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class LeetCode217 {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        LeetCode217 solution = new LeetCode217();
+        boolean result = solution.check(nums);
+        System.out.println(result);
+    }
+}
